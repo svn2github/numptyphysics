897e677e24de28d466c257f8968c28f8cef795fa
tim_edmonds <tim_edmonds@2afca267-d0a4-46bd-89a9-2591803afc3f>
2008-04-17 22:46:33 +0000

    www levels update
    git-svn-id: https://vcs.maemo.org/svn/numptyphysics@31 2afca267-d0a4-46bd-89a9-2591803afc3f

--- www/levels/procnph.sh
+++ www/levels/procnph.sh
@@ -4,7 +4,23 @@ NP='echo' #cd ../../Scratchbox/numptyphysics-0.1 && ./x86_64/Game -bmp'
 NPTPROOT='nptp://numptyphysics.garage.maemo.org/levels/'
 GEOM='200x120'
 
-echo '<html><head><link rel="stylesheet" href="default.css" type="text/css"/></head><body>'
+header()
+{
+I=0
+J=$1
+N=$2
+echo '<html><head>' \
+    '<link rel="stylesheet" href="default.css" type="text/css"/></head><body>' \
+    '<div class=\"header\">' \
+    '<h1><a href="http://numptyphysics.garage.maemo.org">NP</a>-complete</h1>'
+while [ $I -lt $N ] ; do    
+  #echo ' '$I
+  I=$[$I+1];
+done;
+echo '</div>'
+}
+
+header 1 6
 for d in ${BASE}/*; do
   if [ `find $d -name \*.nph | wc -l` -gt 0 ]; then
     echo "<div class=\"collection\"><h2>"`basename $d`"</h2><p>"
@@ -15,7 +31,13 @@ for d in ${BASE}/*; do
 	    #(${NP} $f) && \
 	    convert $f.bmp -geometry ${GEOM} ${THUMB} && rm $f.bmp
 	fi
-	echo "<div class=\"level\"><a href=\"${NPTPROOT}$f\"><img src=\"${THUMB}\"/><br>`basename $f .nph`</a></div>"
+	echo "<div class=\"level\"><a href=\"${NPTPROOT}$f\">" \
+	    "<img src=\"${THUMB}\"/><br>" \
+	    "<span class=\"name\">"`basename $f .nph`"</span></a>" \
+	    "<span class=\"buttons\">" \
+	    "<a href=\"${NPTPROOT}$f\"><img alt=\"play\" src=\"/images/play.gif\"/></a>" \
+	    "<a href=\"$f\"><img alt=\"save\" src=\"/images/save.gif\"/></a>" \
+	    "</span></div>"
     done
     echo "<div class="spacer">&nbsp;</div>"
     echo "</div>"
