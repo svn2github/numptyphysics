cf70fbabbed4a2862c0fb69fd60b0a9170048cf3
tim_edmonds <tim_edmonds@2afca267-d0a4-46bd-89a9-2591803afc3f>
2008-05-29 09:31:35 +0000

    more rss fixes
    git-svn-id: https://vcs.maemo.org/svn/numptyphysics@55 2afca267-d0a4-46bd-89a9-2591803afc3f

--- www/levels/procrss.sh
+++ www/levels/procrss.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-NUM=15
+NUM=0
+MAX=15
 NPTPROOT='nptp://numptyphysics.garage.maemo.org/levels/'
 HTTPROOT='http://numptyphysics.garage.maemo.org/levels/'
 DATE=`date`
@@ -8,20 +9,20 @@ DATE=`date`
 echo '<?xml version="1.0"?><rss version="2.0"><channel>' \
      '<title>NP-complete</title>' \
      '<description>User-contributed NumptyPhysics levels as they happen</description>' \
-     '<link>'$NPTPROOT$'</link>'
+     '<link>'$HTTPROOT$'</link>'
 
-# set $IFS to end-of-line
-IFS=`echo -en "\n\b"`
+IFS=`echo -en "\n\b"` # process log line by line
 
 for line in `svn log -v | egrep '(A .*\.nph$|r[0-9]+.*line)'`; do
   TESTDATE=`echo "$line" | egrep 'r[0-9]+.*line' | cut -d\| -f3 | cut -d\( -f1`
   if [ -n "$TESTDATE" ]; then      
     DATE=$TESTDATE
-  else
+  elif [ $NUM -lt $MAX ]; then
+    NUM=$[$NUM+1]
     LEVEL=`echo "$line" | head -$NUM | cut -d/ -f4-`
     THUMB=thumbs/__`echo $LEVEL | tr /. __`.jpg
     echo '<item><title>'`dirname $LEVEL`': '`basename $LEVEL .nph`'</title>'\
-         '<description>&lt;a href=&quot;'$NPTPROOT$level'&quot;&gt;' \
+         '<description>&lt;a href=&quot;'$NPTPROOT$LEVEL'&quot;&gt;' \
          '&lt;img src=&quot;'$HTTPROOT$THUMB'&quot;&gt;&lt;/a&gt;</description>' \
          '<link>'$NPTPROOT$LEVEL'</link>' \
          '<guid>'$NPTPROOT$LEVEL'</guid>' \
